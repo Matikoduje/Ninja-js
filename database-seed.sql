b216d6724d3c10e8cbca491b51120d80f03c2f11
Matikoduje <mat1986@gmail.com>
2022-10-10 13:41:45 +0200

    Add simple check for DB

--- database-seed.sql
+++ database-seed.sql
@@ -1,12 +1,12 @@
-CREATE TABLE employees
+CREATE TABLE ninjas
 (
     id SERIAL,
     name text,
-    title text,
-    CONSTRAINT employees_pkey PRIMARY KEY (id)
+    weapon text,
+    CONSTRAINT ninjas_pkey PRIMARY KEY (id)
 );
 
-INSERT INTO employees(name, title) VALUES
- ('Meadow Crystalfreak ', 'Head of Operations'),
- ('Buddy-Ray Perceptor', 'DevRel'),
- ('Prince Flitterbell', 'Marketing Guru');
+INSERT INTO ninjas(name, weapon) VALUES
+ ('Meadow Crystalfreak ', 'Katana'),
+ ('Buddy-Ray Perceptor', 'Shuriken'),
+ ('Prince Flitterbell', 'Yari');
--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,20 @@ import express from 'express';
 
 import config from 'config';
 import routes from './routes/ninja';
+import query from './db/database';
 
 const app = express();
 const port: number = config.get('App.port');
 app.use(routes);
 
+const dbQuery = async () => {
+  const { rows } = await query('SELECT * FROM ninjas', []);
+  return rows.length > 0
+    ? 'Database created and connected successfully.'
+    : "Database doesn't work correctly. Please check configuration.";
+};
+
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}...`);
+  const dbHealthCheck = dbQuery;
+  console.log(`${dbHealthCheck} Server is listening on port ${port}...`);
 });
