b2ffa2ccd16b73a6fd3d9ab6306a1736d28a008a
Matikoduje <mat1986@gmail.com>
2022-10-20 14:34:43 +0200

    Changes related to db connection string

--- src/db/database.ts
+++ src/db/database.ts
@@ -2,22 +2,29 @@ import { Pool } from 'pg';
 import config from 'config';
 
 type postgresConfiguration = {
-  host: string;
   password: string;
   user: string;
   database: string;
-  port: number;
+  dbConnectionString: string;
 };
 
-const createConnectionString = (): string => {
+const prepareConnectionString = (): string => {
   const dbPostgres: postgresConfiguration = config.get(
     'App.postgresConfiguration'
   );
-  const { user, password, host, port, database } = dbPostgres;
-  return `postgresql://${user}:${password}@${host}:${port}/${database}`;
+  const { user, password, database, dbConnectionString } = dbPostgres;
+  const replacements: { [key: string]: string } = {
+    '%USER%': user,
+    '%PASSWORD%': password,
+    '%DATABASE%': database
+  };
+
+  return dbConnectionString.replace(/%\w+%/g, (matched) => {
+    return matched in replacements ? replacements[matched] : matched;
+  });
 };
 
-const pool = new Pool({ connectionString: createConnectionString() });
+const pool = new Pool({ connectionString: prepareConnectionString() });
 
 const query = (text: string, params: any) => {
   return pool.query(text, params);
